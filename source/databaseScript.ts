01777e0888d89d92b9702321b405cb354cd7a66d
AnaLLopes <analaura.slopes066@gmail.com>
2023-05-25 15:46:22 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ source/databaseScript.ts
@@ -0,0 +1,14 @@
+import {PrismaClient} from "@prisma/client"
+const prisma = new PrismaClient();
+
+prisma.user.create({
+    data:{
+        nome: "Leonardo",
+        sobrenome: "Forner",
+        curso:"Engenharia de Software",
+        email: "L.forner@edu.pucrs.br",
+        username:"21100996",
+        password:"123",
+        ano_entrada:"2021"
+    }
+})
