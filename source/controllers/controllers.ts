858548227ce1aba6c06e44487792f67a04bd8340
leonardolforner <leonardoforner@gmail.com>
2023-05-29 02:43:45 -0300

    api finalizada

--- source/controllers/controllers.ts
+++ source/controllers/controllers.ts
@@ -11,4 +11,59 @@ const getAllUsers =  async (req: Request, res: Response) => {
     
 }
 
-export default {getAllUsers}
+const getUser = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    let user = await prisma.user.findFirst({
+        where:{
+            id: id
+        }
+    })
+    return res.json({
+        message: user
+    })
+}
+
+const updatePassword = async (req:Request, res:Response) => {
+    const id = req.params.id;
+    const newPassword = req.body.password;
+    await prisma.user.update({
+        where: {
+            id:id
+        },
+        data:{
+            password: newPassword
+        }
+    })
+}
+
+const createUser = async (req:Request, res:Response) => {
+    const name = req.body.name
+    const lastName = req.body.lasatName;
+    const course = req.body.course;
+    const email = req.body.email;
+    const username = req.body.username;
+    const password = req.body.password;
+    const year = req.body.year;
+    await prisma.user.create({
+        data:{
+            nome: name,
+            sobrenome: lastName,
+            curso: course,
+            email: email,
+            username: username,
+            password: password,
+            ano_entrada: year
+        }
+    })
+}
+
+const deleteUser = async (req:Request, res:Response) => {
+    const id = req.params.id;
+    await prisma.user.delete({
+        where:{
+            id: id
+        }
+    })
+}
+
+export default {getAllUsers, getUser, updatePassword, createUser, deleteUser}
--- source/routes/routes.ts
+++ source/routes/routes.ts
@@ -3,5 +3,9 @@ import controller from '../controllers/controllers';
 const router = express.Router();
 
 router.get("/user/", controller.getAllUsers);
+router.get("/user/:id/", controller.getUser);
+router.put("user/changePassword/:id/", controller.updatePassword);
+router.post("user/new/", controller.createUser);
+router.delete("user/delete/:id/", controller.deleteUser)
 
 export default router
